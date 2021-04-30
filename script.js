577289ee626ba36815c7a7241980d31857832235
Kauan Lazzarin <kauanlazzarin@gmail.com>
2021-04-29 22:06:14 -0300

    comments add

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /* 
     Pegar elementos do html
+    e iniciar variáveis
 */
 
 const buttons = document.querySelectorAll('.calc-number');
@@ -8,11 +9,16 @@ const resultButton = document.querySelector('#calc-result');
 const display = document.querySelector('#display');
 const operatorDisplay = document.querySelector('#operator-display');
 
-let operatorValue;
+let operatorValue ='';
 let values = [];
-let firstValue;
-let secondValue;
+let firstValue = 0;
+let secondValue = 0;
 
+/* 
+    Para cada botão de valor
+    adicionar valores ao array de valores
+    e mudar o valor do display
+*/
 buttons.forEach((button) => {
     button.onclick = () => {
         values.push(button.innerText);
@@ -20,6 +26,11 @@ buttons.forEach((button) => {
     };
 });
 
+/* 
+    Ao clicar em um botão de operador 
+    limpar o array e realizar a soma dos 
+    primeiros valores digitados
+*/
 operators.forEach((operator) => {
     operator.onclick = () => {
         firstValue = parseInt(values.reduce((value1, value2) => value1 + value2));
@@ -30,12 +41,22 @@ operators.forEach((operator) => {
     };
 });
 
+/* 
+    Ao clicar no botão de resultado iterar
+    os valores do segundo valor, obter o 
+    resultado e apresentar no display
+*/
 resultButton.onclick = () => {
-    secondValue = parseInt(values.reduce((value1, value2) => value1 + value2));
+    secondValue = parseInt(values.reduce((value1, value2) => value1 + value2))
     const result = getResult(operatorValue);
+    firstValue = result;
     changeDisplayValue(result);
 };
 
+/* 
+    Função que verifica o operador
+    e realiza as operações 
+*/
 function getResult (operator) {
     let finalValue;
 
@@ -52,6 +73,10 @@ function getResult (operator) {
     return finalValue;
 };
 
+
+/* 
+    Função para atualizar o valor do display
+*/
 function changeDisplayValue (displayValue) {
     display.value = displayValue;
 };
